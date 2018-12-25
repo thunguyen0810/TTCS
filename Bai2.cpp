8277230766a171a8a3b32e7025359fedb6aa5662
ThuNguyen <thunguyen1918@gmail.com>
2018-12-25 21:01:25 +0700

    Update Bai2.cpp

--- Bai2.cpp
+++ Bai2.cpp
@@ -4,28 +4,28 @@ using namespace std;
 
 typedef struct NgayThang
 {
-	int d;
-	int m;
-	int y;
+	int ngay;
+	int thang;
+	int nam; 
 } NgayThang;
 
-//Ham nhap ngay thang nam 
+//Ham nhap ngay thang nam
 void Nhap(string &s, NgayThang &ngay)
 {
-	cout << "Nhap ngay, thang, nam : ";
+	cout << "Nhap ngay thang nam : ";
 	getline(cin, s);
 	cout << s << endl;
 }
 
-//Chuyen chuoi ngay thang nam thanh kieu so
+//Chuyen doi kieu chuoi thanh kieu so
 void LayNgay(string &s, NgayThang &ngay)
 {
-	ngay.d = atoi((s.substr(0, 2)).c_str());
-	cout << ngay.d << " ";
-	ngay.m = atoi((s.substr(3, 2)).c_str());
-	cout << ngay.m << " ";
-	ngay.y = atoi((s.substr(6, 4)).c_str());
-	cout << ngay.y << endl;
+	ngay.ngay = atoi((s.substr(0, 2)).c_str());
+	cout << ngay.ngay << " ";
+	ngay.thang = atoi((s.substr(3, 2)).c_str());
+	cout << ngay.thang << " ";
+	ngay.nam = atoi((s.substr(6, 4)).c_str());
+	cout << ngay.nam << endl;
 }
 
 //Kiem tra nam nhuan
@@ -33,67 +33,23 @@ int KiemTra(int nam)
 {
 	if((nam % 400 == 0) || (nam % 4 == 0 && nam % 100 != 0))
 		return 1;
-	else 
+	else
 		return 0;
 }
 
-//Tinh so thu tu cua ngay
+//Tinh so thu tu cua ngay trong nam
 
 int Thang[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int TinhSTT(NgayThang n)
 {
 	int stt = 0;
-	for(int i = 0; i < n.m; i++)
-	{
+	for(int i = 0; i < n.thang; i++)
 		stt += Thang[i];
-		stt += n.d;
-	}
-	if(KiemTra && n.m > 2)
+	stt += n.ngay;
+	if(KiemTra(n.nam) && n.thang > 2)
 		stt + 1;
 	return stt;
-}
-
-int STTNgayTrongNam(NgayThang n);
-
-NgayThang NgayTuSTT(int stt, int nam)
-{
-	int t = 1;
-	int ngay = stt;
-	int kt = KiemTra(nam);
-	while(stt > 0)
-	{
-		if(kt == 1 && t == 2)
-			stt = stt - 29;
-		else 
-			stt = stt - Thang[t];
-		if(stt > 0)
-		{
-			ngay = stt;
-			t++;
-		}
-	}
-	NgayThang nt;
-	nt.d = ngay;
-	nt.m = t;
-	nt.y = nam;
-	return nt;
-}
-
-NgayThang CongNgayThang(NgayThang n, int x)
-{
-	int kq = x + STTNgayTrongNam(n);
-	int nam = n.y;
-	if(kq <= 365)
-		return NgayTuSTT(kq, n.y);
-	else
-		if(KiemTra(n.y) == 1)
-			if(kq == 366)
-				return NgayTuSTT(kq, n.y);
-			else
-				return NgayTuSTT(kq - 366, nam + 1);
-		else
-			return NgayTuSTT(kq - 365, nam + 1);
 }
 
 int main()
@@ -102,7 +58,9 @@ int main()
 	string s;
 	Nhap(s, ngay);
 	LayNgay(s, ngay);
-	cout << "La ngay thu " << TinhSTT(ngay) << " trong nam";
-	cout << NgayTuSTT(TinhSTT(ngay), );
-	cout << CongNgayThang(s, ngay);
+	if(KiemTra(ngay.nam) == 1)
+		cout << "Nam " << ngay.nam << " la nam nhuan" << endl;
+	else
+		cout << "Nam " << ngay.nam << " khong la nam nhuan" << endl;
+	cout << "La ngay thu " << TinhSTT(ngay) << " trong nam" << endl;
 }
