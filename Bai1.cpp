504ab582ea100e4d1789e89d7abeccf6d5c1bf22
ThuNguyen <thunguyen1918@gmail.com>
2018-12-26 11:08:56 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -1,73 +1,85 @@
 #include<iostream>
 #include<vector>
 #include<fstream>
-#include<string>
+#include<string.h>
 using namespace std;
 
 typedef struct Contact
 {
-	string ten;
+	char ten[30];
 	char sdt[11];
 	string gioitinh, email, diachi;
 } Contact;
 
+char* fileName = "danhba.dat";
+
 vector<Contact> db;
-ifstream fi("input.dat");
 
-char data[100];
+void DocDBTuFile();
+void GhiDBVaoFile();
+void ThemMoi(Contact c);
+void inContact(Contact c);
+void inDB();
 
-void DocFile()
+int main()
 {
-	FILE *f = fopen("input.dat", "rb");
+	DocDBTuFile();
+	//inDB();
+	Contact c;
+	strcpy(c.sdt, "0338718146");
+	strcpy(c.ten, "Nguyen Phan Anh Thu");
+	//ThemMoi(c);
+	//inDB();
+}
+
+void DocDBTuFile()
+{
+	Contact c;
+	db.clear();
+	FILE *f;
+	f = fopen(fileName, "rb");
+	if(f != NULL)
+	{
+		while(!feof(f))
+		{
+			Contact c;
+			fread(&c, sizeof(Contact), 1, f);
+			db.push_back(c);
+		}
+		fclose(f);
+		db.push_back(c);
+	}
+}
+
+void GhiDBVaoFile()
+{
+	int size = db.size();
+	FILE *f;
+	f = fopen(fileName, "wb");
+	Contact c;
+	for(int i = 0; i < size; i++)
+	{
+		c = db[i];
+		fwrite(&c, sizeof(Contact), 1, f);
+	}
+	fclose(f);
 }
 
 void ThemMoi(Contact c)
 {
 	db.push_back(c);
-	
+	GhiDBVaoFile();
 }
 
-void Ghidb()
+void inContact(Contact c)
 {
-	for(int i = 0; i < db.size(); i++)
-		f --> (&db[i], sizeof(db), 1, f)
+	cout << "So dien thoai " << c.sdt << endl;
+	cout << "Ten " << c.ten << endl;
 }
 
-void LietkeDB()
+void inDB()
 {
-	DocFile();
-	for(int i = 0; i < sizeof(db); i++)
-		Xuat(db[i]);
-}
-
-void CapNhat(Contact c)
-{
-	//Tim contact m co so dien thoai c.sdt
-	//Cap nhat c vao m
-	//Ghi danh ba vao file
-}
-
-void XoaContact(char *sdt)
-{
-	//Tim contact m co trong danh ba
-	//Xoa m
-	//Ghi danh ba vao file
-}
-
-vector <Contact> TimTheoTen(char *ten)
-{
-	//Tim cac contact m co ten "ten"
-	//Them m vao bien vector
-	//Tra ve bien vector
-}
-
-int main()
-{
-	Contact c;
-	strcpy(c.ten, "Nguyen Phan Anh Thu");
-	c.gioitinh = 1;
-	strcpy(c.sdt, "0338718146");
-	strcpy(c.email, "thunguyen1918@gmail.com");
-	strcpy(c.diachi, "Nha Trang");
-	cout << c.ten
+	int size = db.size();
+	for(int i = 0; i < size; i++)
+		inContact(db[i]);
 }
