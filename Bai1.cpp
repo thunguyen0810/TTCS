5a46191d8cb06e7854d4d24c2b9d4728bcb9172e
ThuNguyen <thunguyen1918@gmail.com>
2018-12-27 09:13:55 +0700

    Update Bai1.cpp

--- Bai1.cpp
+++ Bai1.cpp
@@ -5,7 +5,7 @@
 #include<stdlib.h>
 using namespace std;
 
-typedef struct Contact
+typedef struct
 {
 	char ten[20];
 	bool gioitinh;
@@ -13,13 +13,55 @@ typedef struct Contact
 } Contact;
 
 vector<Contact> DB;
-vector<Contact> db;
+
+void ThemMoi()
+{
+	Contact c;
+	
+	strcpy(c.ten, "Nguyen Phan Anh Thu");
+	strcpy(c.sdt, "123");
+	c.gioitinh = 0;
+	strcpy(c.email, "abc");
+	strcpy(c.diachi, "NT");
+	DB.push_back(c);
+	
+	
+	strcpy(c.ten, "Tran Thi Ngoc Tram");
+	strcpy(c.sdt, "456");
+	c.gioitinh = 1;
+	strcpy(c.email, "xyz");
+	strcpy(c.diachi, "QN");
+	DB.push_back(c);
+	
+	strcpy(c.ten, "Tu");
+	strcpy(c.sdt, "789");
+	c.gioitinh = 1;
+	strcpy(c.email, "lmn");
+	strcpy(c.diachi, "BA");
+	DB.push_back(c);
+}
 
 void GhiDBVaoFile()
 {
 	FILE *f = fopen("input.dat", "wb");
-	for(int i = 0; i < db.size(); i++)
-		fwrite(&DB[i], sizeof(Contact), 1, f);
+	Contact db;
+	for(int i = 0; i < DB.size(); i++) {
+		db = DB[i];
+		fwrite(&db, sizeof(Contact), 1, f);
+	}
+	fclose(f);
+}
+
+void DocDBTuFile()
+{
+	FILE *f = fopen("input.dat", "rb");
+	if(f != NULL)
+		while(!feof(f))
+		{
+			Contact c;
+			fread(&c, sizeof(Contact), 1, f);
+			DB.push_back(c);
+		}
 	fclose(f);
 }
 
@@ -30,92 +72,68 @@ void InDanhBa(Contact c)
 	cout << "Gioi tinh : " << c.gioitinh << endl;
 	cout << "Email : " << c.email << endl;
 	cout << "Dia chi : " << c.diachi << endl;
-}
-
-void ThemMoi()
-{
-	char str1[] = "Vi du chuoi";
-	Contact c;
-	strcpy(c.ten, "Nguyen Phan Anh Thu");
-	strcpy(c.sdt, str1);
-	c.gioitinh = 0;
-	strcpy(c.email, str1);
-	strcpy(c.diachi, str1);
-	DB.push_back(c);
-	strcpy(c.ten, str1);
-	strcpy(c.sdt, "0338718146");
-	c.gioitinh = 1;
-	strcpy(c.email, str1);
-	strcpy(c.diachi, str1);
-	DB.push_back(c);
-	strcpy(c.ten, "Thu");
-	strcpy(c.sdt, "0338148562");
-	c.gioitinh = 1;
-	strcpy(c.email, "thunguyen1918@gmail.com");
-	strcpy(c.diachi, str1);
-	DB.push_back(c);
-}
-
-void DocDBTuFile()
-{
-	FILE *f = fopen("input.dat", "rb");
-	for(int i = 0; i < db.size(); i++)
-	{
-		Contact c;
-		fread(&c, sizeof(c), 1, f);
-		DB.push_back(c);
-	}
-	fclose(f);
+	cout << "-------------------------------//-------------------" << endl;
 }
 
 void LietKe()
 {
-	for(int i = 0; i < db.size(); i++)
+	for(int i = 0; i < DB.size(); i++)
 	{
 		InDanhBa(DB[i]);
 	}
 }
 
-void ChinhSua(char tt[])
-{
-	for(int i = 0; i < db.size(); i++)
-		if(strcmp(DB[i].sdt, tt) == 0)
-			strcpy(DB[i].ten, "Pham Duy Uyen Phuong");
-}
-
-void Xoa(char t[])
-{
-	for(int i = 0; i < db.size(); i++)
-	{
-		if(strcmp(DB[i].sdt, t) == 0)
-			DB.erase(DB.begin() + i);
-	}
-}
-
-Contact TimKiem(char t[])
-{
-	for(int i = 0; i < db.size(); i++)
-	{
-		if(strcmp(DB[i].sdt, t) == 0)
-			return DB[i];
-	}
-}
+//void ChinhSua(char tt[])
+//{
+//	for(int i = 0; i < DB.size(); i++)
+//	{
+//		Contact c;
+//		c = DB[i];
+//		if(strcmp(c.sdt, tt) == 0)
+//			strcpy(c.ten, "Pham Duy Uyen Phuong");
+//	}
+//		
+//}
+//
+//void Xoa(char t[])
+//{
+//	for(int i = 0; i < DB.size(); i++)
+//	{
+//		if(strcmp(DB[i].sdt, t) == 0)
+//			DB.erase(DB.begin() + i);
+//	}
+//}
+//
+//Contact TimKiem(char t[])
+//{
+//	for(int i = 0; i < DB.size(); i++)
+//	{
+//		if(strcmp(DB[i].sdt, t) == 0)
+//			return DB[i];
+//	}
+//}
 
 int main()
 {
 	ThemMoi();
 	GhiDBVaoFile();
 	DocDBTuFile();
-	ChinhSua("0338718144");
-	cout << "Danh ba sau khi chinh sua : " << endl;
-	for(int i = 0; i < db.size(); i++)
-		InDanhBa(db[i]);
-	Xoa("Nguyen Phan Anh Thu");
-	cout << "Danh ba sau khi xoa : " << endl;
-	for(int i = 0; i < db.size(); i++)
-		InDanhBa(db[i]);
-	Contact c = TimKiem("Thu");
-	cout << "Contact muon tim la : " << endl;
-		InDanhBa(c);
+	LietKe();
+	cout << "--------------------------------------------------" << endl;
+	
+//	ChinhSua("123");
+//	cout << "Danh ba sau khi chinh sua : " << endl;
+//	LietKe();
+//	cout << "--------------------------------------------------" << endl;
+//	
+//	Xoa("Nguyen Phan Anh Thu");
+//	cout << "Danh ba sau khi xoa : " << endl;
+//	LietKe();
+//	cout << "--------------------------------------------------" << endl;
+//	
+//	Contact c = TimKiem("Tu");
+//	cout << "Contact : " << endl;
+//	LietKe();
+//	cout << "--------------------------------------------------" << endl;
 }
 
