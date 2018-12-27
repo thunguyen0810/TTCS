5b159be7fd6dbb0e5c0b2cc9aa1a1eb7f6ce1268
ThuNguyen <thunguyen1918@gmail.com>
2018-12-27 21:17:44 +0700

    Update Bai2.cpp

--- Bai2.cpp
+++ Bai2.cpp
@@ -9,26 +9,31 @@ typedef struct NgayThang
 	int nam; 
 } NgayThang;
 
-//Ham nhap ngay thang nam
-void Nhap(string &s, NgayThang &ngay)
+//Ham nay dung de nhap ngay thang nam
+//s : chuoi ngay thang nam
+void Nhap(string &s)
 {
 	cout << "Nhap ngay thang nam : ";
-	getline(cin, s);
+	getline(cin, s); // Nhap chuoi ngay thang nam
 	cout << s << endl;
 }
 
-//Chuyen doi kieu chuoi thanh kieu so
+//Ham nay dung de chuyen doi kieu chuoi cua ngay thang nam thanh kieu so
+//s : chuoi ngay thang nam
+// ngay : sau khi chuyen kieu chuoi cua ngay thang nam thanh so thi dung bien ngay de gan cho cac tham so cua struct
 void LayNgay(string &s, NgayThang &ngay)
 {
-	ngay.ngay = atoi((s.substr(0, 2)).c_str());
+	ngay.ngay = atoi((s.substr(0, 2)).c_str()); // substr dung de tra ve chuoi con
 	cout << ngay.ngay << " ";
-	ngay.thang = atoi((s.substr(3, 2)).c_str());
+	ngay.thang = atoi((s.substr(3, 2)).c_str()); // atoi dung de chuyen kieu chuoi ve kieu so
 	cout << ngay.thang << " ";
 	ngay.nam = atoi((s.substr(6, 4)).c_str());
 	cout << ngay.nam << endl;
 }
 
-//Kiem tra nam nhuan
+//Ham dung de kiem tra nam nhuan
+//nam : nam can kiem tra
+//Neu nam can kiem tra la nam nhuan thi tra ve 1 con nguoc lai khong phai nam nhuan thi tra ve 0
 int KiemTra(int nam)
 {
 	if((nam % 400 == 0) || (nam % 4 == 0 && nam % 100 != 0))
@@ -37,10 +42,12 @@ int KiemTra(int nam)
 		return 0;
 }
 
-//Tinh so thu tu cua ngay trong nam
-
+//Khai bao mang thang
 int Thang[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+//Ham dung de tinh so thu tu cua ngay trong nam
+//n : bien dung de gan cho cac tham so cua struct
+//Ham nay tra ve so thu tu cua ngay trong nam
 int TinhSTT(NgayThang n)
 {
 	int stt = 0;
@@ -52,6 +59,10 @@ int TinhSTT(NgayThang n)
 	return stt;
 }
 
+//Ham dung de chuyen so thu tu cua ngay trong nam thanh kieu ngay thang nam
+//stt : so thu tu cua ngay trong nam
+//nam : bien nay dung de kiem tra xem nam co nhuan hay khong
+//Ham nay tra ve kieu ngay thang nam
 NgayThang NgayTuSTT(int stt, int nam)
 {
 	int t = 1;
@@ -76,6 +87,10 @@ NgayThang NgayTuSTT(int stt, int nam)
 	return nt;
 }
 
+//Ham nay dung de cong mot so nguyen duong vao ngay
+//n : bien dung de gan cho cac tham so cua struct
+//x : so nguyen duong de cong vao ngay
+//Ham nay tra ve kieu ngay thang nam
 NgayThang CongNgayThang(NgayThang n, int x)
 {
 	int kq = x + TinhSTT(n);
@@ -97,7 +112,7 @@ int main()
 	NgayThang ngay;
 	string s;
 	int x;
-	Nhap(s, ngay);
+	Nhap(s);
 	LayNgay(s, ngay); 
 	cout << "Nhap so nguyen duong : ";
 	cin >> x;
