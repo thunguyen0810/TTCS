30634cb07bd168435a365ce68450b3f5143b4a0e
ThuNguyen <thunguyen1918@gmail.com>
2018-12-27 18:06:50 +0700

    Update bai3.cpp

--- bai3.cpp
+++ bai3.cpp
@@ -21,37 +21,89 @@ typedef struct
 vector <CH_DA> dscau;
 vector <Nguoi> danhsach;
 
-int KiemTra(string A, string B);
-
-//Xuat cau hoi
-void Xuat(CH_DA ch)
+int KiemTra(string A, string B)
 {
-	string s;
-	cout << "Nhap ten nguoi choi : ";
-	cin >> s;
-	cout << "--------------------" << endl;
-	
-	string dapan;
-	int diem;
-	cout << ch.cauhoi << endl;
-	cout << ch.A << endl;
-	cout << ch.B << endl;
-	cout << ch.C << endl;
-	cout << ch.D << endl;
-	cout << "Nhap dap an : ";
-	cin >> dapan;
-	if(KiemTra(ch.DA, dapan) == 2)
+	if(A.compare(B) == 0)
 	{
 		cout << "Dap an dung !" << endl;
-		diem += KiemTra(ch.DA, dapan);
+		cout << "--------------------"<< endl;
+		return 2;
 	}
 	else
+	{
 		cout << "Dap an sai !" << endl;
-		cout << "-------//---------" << endl;
-	dscau.push_back(ch);
+		cout << "--------------------"<< endl;
+		return 0;
+	}
 }
 
-void Xuat1()
+//Kiem tra xem dap an
+//A: dap an trong cau hoi
+//B: dap an cua nguoi choi
+//Ham nay tra ve 2 neu tra loi dung, neu tra loi sai thi tra ve 0
+void LuuDiem(string ten, int diem)
+{
+	fstream f1;
+	f1.open("luudiem.txt", ios::out | ios::app);
+	f1 << ten << endl << diem ;
+	f1.close();
+}
+
+//Lay cau hoi ra tu file
+void LayDe()
+{
+	ifstream f("de.txt");
+	int sl;
+	int diem = 0;
+	string s, ans, ten;
+	CH_DA ch;
+	getline(f,s); 
+	sl = atoi(s.c_str());
+	
+	cout << "Nhap ten cua nguoi choi : " ;
+	cin >> ten;
+	getline(cin, ten);
+	fflush(stdin);
+	cout << "--------------------" << endl;
+	
+	for(int i = 0; i < sl; i++)
+	{
+		CH_DA qs;
+		getline(f,s);
+		qs.cauhoi = s;
+		cout << qs.cauhoi << endl;
+			
+		getline(f, s);
+		qs.A = s;
+		cout << qs.A << endl;
+		
+		getline(f, s);
+		qs.B = s;
+		cout << qs.B << endl;
+		
+		
+		getline(f, s);
+		qs.C = s;
+		cout << qs.C << endl;
+		
+		getline(f, s);
+		qs.D = s;
+		cout << qs.D << endl;
+		
+		getline(f, s);
+		qs.DA = s;
+		cout << "Nhap dap an : ";
+		getline(cin, ans);
+		if(KiemTra(qs.DA, ans) == 2)
+			diem += 2;
+		dscau.push_back(qs);
+	}
+	cout << "Diem cua ban la : " << diem << endl;
+	fflush(stdin);
+	LuuDiem(ten, diem);
+}
+
+void XemDiem()
 {
 	string s;
 	ifstream f1("luudiem.txt");
@@ -65,73 +117,8 @@ void Xuat1()
 	f1.close();
 }
 
-//Doc cau hoi trong file
-void DocDe()
-{
-	for(int i = 0; i < dscau.size(); i++)
-		Xuat(dscau[i]);
-}
-
-//Lay cau hoi ra tu file
-void LayDe()
-{
-	ifstream f("de.txt");
-	int sl;
-	string s, ans;
-	CH_DA ch;
-	getline(f,s);
-	sl = atoi(s.c_str());
-	for(int i = 0; i < sl; i++)
-	{
-		CH_DA qs;
-		getline(f,s);
-		qs.cauhoi = s;
-			
-		getline(f, s);
-		qs.A = s;
-		
-		getline(f, s);
-		qs.B = s;
-		
-		getline(f, s);
-		qs.C = s;
-		
-		getline(f, s);
-		qs.D = s;
-		
-		getline(f, s);
-		qs.DA = s;
-
-		dscau.push_back(qs);
-	}
-}
-
-//Kiem tra xem dap an
-//A: dap an trong cau hoi
-//B: dap an cua nguoi choi
-//Ham nay tra ve 2 neu tra loi dung, neu tra loi sai thi tra ve 0
-int KiemTra(string A, string B)
-{
-	if(A.compare(B) == 0)
-		return 2;
-	else
-		return 0;
-}
-
-void LuuDiem(string s, int diem)
-{
-	fstream f1;
-	f1.open("luudiem.txt", ios::out | ios::app);
-	f1 << s << endl << diem ;
-	f1.close();
-}
-
 int main()
 {
-	string s;
-	int diem;
-	fflush(stdin);
 	LayDe();
-	DocDe();
-	LuuDiem(s, diem);
+//	XemDiem();
 }
