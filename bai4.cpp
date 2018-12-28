7bbf4296b1574ba78e028586a8cf4fda3ec8a56a
ThuNguyen <thunguyen1918@gmail.com>
2018-12-28 09:20:17 +0700

    Update bai4.cpp

--- bai4.cpp
+++ bai4.cpp
@@ -27,9 +27,30 @@ void XuatDoThi(int a[10][10])
 	}	
 }
 
+void TaoMoi(int A[10][10], int &n)
+{
+	cout << "Nhap n : ";
+	cin >> n;
+	f.open("dothi.txt", ios::out);
+	f << n << endl;
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "trong so tu " << i << " den " << j << " : ";
+			cin >> A[i][j];
+			f << A[i][j] << " ";
+		}
+		f << endl;
+	}
+	f.close();
+}
+
 int main()
 {
 	int A[10][10];
+	int n;
+	TaoMoi(A, n);
 	DocFile(A);
 	XuatDoThi(A);
 }
